5ca0d6f6e371e3d4067e43efe549b769d63d0a0a
Josafath <kevincc@live.com>
2026-01-22 11:49:36 -0600

    Cambio de direcciones pero hare una pausa para aprender demás cosas que necesito

--- Curso2_Avanzado/resources/partials/navbar.php
+++ Curso2_Avanzado/resources/partials/navbar.php
@@ -1,10 +1,10 @@
 <nav class="bg-gray-800">
     <div class="mx-auto max-w-7xl flex h-16 items-center justify-center">
         <div class="flex gap-4">
-            <a href="/Curso2_Avanzado/public/"      class="<?= $_SERVER['REQUEST_URI'] === '/Curso2_Avanzado/public/'      ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-600 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Inicio</a>
-            <a href="/Curso2_Avanzado/public/post"  class="<?= $_SERVER['REQUEST_URI'] === '/Curso2_Avanzado/public/post'  ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-600 hover:text-white' ?>   rounded-md px-3 py-2 text-sm font-medium">Post</a>
-            <a href="/Curso2_Avanzado/public/about" class="<?= $_SERVER['REQUEST_URI'] === '/Curso2_Avanzado/public/about' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-600 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Acerca de</a>
-            <a href="/Curso2_Avanzado/public/links" class="<?= $_SERVER['REQUEST_URI'] === '/Curso2_Avanzado/public/links' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-600 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Proyectos</a>
+            <a href="/public/"      class="<?= $_SERVER['REQUEST_URI'] === '/public/'      ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-600 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Inicio</a>
+            <a href="/public/post"  class="<?= $_SERVER['REQUEST_URI'] === '/public/post'  ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-600 hover:text-white' ?>   rounded-md px-3 py-2 text-sm font-medium">Post</a>
+            <a href="/public/about" class="<?= $_SERVER['REQUEST_URI'] === '/public/about' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-600 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Acerca de</a>
+            <a href="/public/links" class="<?= $_SERVER['REQUEST_URI'] === '/public/links' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-600 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Proyectos</a>
         </div>
     </div>
 </nav>
--- Curso2_Avanzado/routes/web.php
+++ Curso2_Avanzado/routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    '/Curso2_Avanzado/public/' => 'app/Controllers/home.php',
-    '/Curso2_Avanzado/public/post' => 'app/Controllers/post.php',
-    '/Curso2_Avanzado/public/about' => 'app/Controllers/about.php',
-    '/Curso2_Avanzado/public/links' => 'app/Controllers/links.php',
+    '/public/' => 'app/Controllers/home.php',
+    '/public/post' => 'app/Controllers/post.php',
+    '/public/about' => 'app/Controllers/about.php',
+    '/public/links' => 'app/Controllers/links.php',
 ];
